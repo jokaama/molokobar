1918a9796c0555233af4dab8c32f73f97bb2fa7c
Anne-Charlotte <ac.drevensek.deldicque@gmail.com>
2020-02-19 11:48:01 +0100

    ajout de l'interrupteur pour allumer la diode

--- script_arduino.ino
+++ script_arduino.ino
@@ -1,11 +1,33 @@
-// Allumer une LED branchée sur la broche 5
+/*
+
+ Allume une LED branchée sur la broche 13 lorsque le bouton
+ connecté sur la broche 2 est appuyé.
+ */
+
+// Initialisation des constantes :
+const int bouton = 2;     // Numéro de la broche à laquelle est connecté le bouton poussoir
+const int led =  5;      // Numéro de la broche à laquelle est connectée la LED
+
+// Déclaration des variables :
+int etatBouton = 0;         // variable qui sera utilisée pour stocker l'état du bouton// Allumer une LED branchée sur la broche 5
+
 void setup() {
-  pinMode(5,OUTPUT);
+    // indique que la broche led est une sortie :
+  pinMode(led,OUTPUT);
+  // indique que la broche bouton est une entrée :
+  pinMode(bouton, INPUT);
 }
-// Faire clignoter la LED par intervalle de 1000ms
+
 void loop(){
-  digintalWrite(5,LOW);
-  delay(1000);
-  digitalWrite(5,HIGH);
-  delay(1000);
+  // lit l'état du bouton et stocke le résultat dans etatBouton
+  etatBouton = digitalRead(bouton);
+  // Si etatBouton est à 5V (HIGH) c'est que le bouton est appuyé
+  if (etatBouton == HIGH) {
+    // on allume la LED
+  digintalWrite(led,HIGH);
+  }
+  else {
+    // sinon on éteint
+  digitalWrite(led,LOW);
+  }
 }
